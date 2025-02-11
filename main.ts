f08d1b0d33f175696d6f2753bd28d6cc811153f3
Axelclouet <axelclouet4064@gmail.com>
2025-02-11 08:56:56 +0100

    Mise à jour pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
- 
+let mySprite = sprites.create(assets.image`jouer`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`zombie`, SpriteKind.Player)
+let mySprite3 = sprites.create(assets.image`myImage`, SpriteKind.Player)
